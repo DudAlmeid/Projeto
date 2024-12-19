358c048db1c35d4dfb7aba0a590d500d1bd5e0ee
Eduarda de Almeida <157630819+DudAlmeid@users.noreply.github.com>
2024-12-18 21:23:36 -0300

    Add files via upload

---
+++ sol e trilha/script.js
@@ -0,0 +1,10 @@
+// Função para scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
